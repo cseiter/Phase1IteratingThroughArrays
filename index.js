4bc178a9a2d76732884345025e0afec11a7a926c
chris <cseiter@gmail.com>
2021-04-03 19:53:59 -0400

    passes all tests

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+function writeCards(arrNames,event) {
+	let TYCards = []
+	for (let i = 0; i < arrNames.length; i++) {
+		TYCards.push(`Thank you, ${arrNames[i]}, for the wonderful ${event} gift!`)
+		}
+	return TYCards
+	}
+
+function countDown(start) {
+	while (start > -1) {
+		console.log(start);
+		start--;
+		}
+	}
